6b10a3bda7bda07970c7e8d8ed7788ad7a8a19c3
Adam Foreman <adam.foreman@gmail.com>
2019-06-16 20:47:54 +1000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import RandomizationEngine from './RandomizationEngine/RandomizationEngine.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handleRandomizeClick(event) {
+    console.log('handleRandomizeClick(');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <input type="button" value="Randomize fonts!" onClick={this.handleRandomizeClick.bind(this)} />
+        </div>
+        <RandomizationEngine />
+      </div>
+    );
+  }
 }
 
 export default App;
